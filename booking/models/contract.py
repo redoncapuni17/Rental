805e087c9b9c4057226a8395c0b39042c3584b56
redon <redoncapuni@gmail.com>
2025-06-19 19:10:34 +0200

    some new field are added in contract and maintennance

--- booking/models/contract.py
+++ booking/models/contract.py
@@ -5,10 +5,15 @@ class RentalContract(models.Model):
     _name = 'rental.contract'
     _description = 'Contract Description'
 
-    name = fields.Char(string="Contract Reference", required=True, copy=False, readonly=True, default='New')
+    name = fields.Char(string="Contract", required=True,)
     booking_id = fields.Many2one('rental.booking', string="Booking", required=True, ondelete='cascade')
     customer_id = fields.Many2one('rental.customer', string="Customer", related='booking_id.customer_id', readonly=True)
     car_id = fields.Many2one('rental.car', string="Car", related='booking_id.car_id', readonly=True)
     start_date = fields.Datetime(string="Start Date", related='booking_id.start_date', readonly=True)
     end_date = fields.Datetime(string="End Date", related='booking_id.end_date', readonly=True)
     total_price = fields.Float(string="Total Price", related='booking_id.total_price', readonly=True)
+
+    license_plate = fields.Char(string="License Plate", related='car_id.license_plate', readonly=True)
+    category_id = fields.Many2one('rental.category', string='Category', related='car_id.category_id', readonly=True)
+    fuel_type = fields.Selection(related='car_id.fuel_type', string='Fuel Type', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
--- maintenances/models/maintenance.py
+++ maintenances/models/maintenance.py
@@ -8,10 +8,11 @@ class RentalMaintenance(models.Model):
     name = fields.Char(string='Maintenance Reference', required=True)
     car_id = fields.Many2one('rental.car', string='Car', required=True)
     maintenance_date = fields.Date(string='Maintenance Date', required=True, default=fields.Date.context_today)
-    description = fields.Text(string='Description')
+    description = fields.Char(string='Description')
     cost = fields.Float(string='Cost')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('done', 'Done'),
         ('cancelled', 'Cancelled'),
     ], default='draft', string='Status')
+    notes = fields.Text(string='Notes')
